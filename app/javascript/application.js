d0c9be973ed2040b100cbb42f4e6f43828e37d25
Kendall McHenry <kendall.mchenry@ramseysolutions.com>
2023-06-29 14:02:25 -0500

    Removed reference to Webpacker & changed to jsbundling-rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
+// Entry point for the build script in your package.json
+import "./channels"
+import "./controllers"
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -6,7 +9,6 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
 
 Rails.start()
 Turbolinks.start()

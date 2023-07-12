350dd2cd5293bf010c3ae15dc9eaea93f34cb9d6
Kendall McHenry <kendall.mchenry@ramseysolutions.com>
2023-07-12 14:54:21 -0500

    Fix validation handling, add flashes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,8 +14,18 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    @movie.update(movie_params)
-    redirect_to @movie
+    # @movie.update(movie_params)
+    # redirect_to @movie
+
+    respond_to do |format|
+      if @movie.update(movie_params)
+        format.html { redirect_to movie_url(@movie), notice: "Movie was successfully updated." }
+        format.json { render :show, status: :ok, location: @movie }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @movie.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def new
@@ -24,17 +34,32 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new(movie_params)
-    if @movie.save
-      redirect_to @movie
-    else
-      render :new
+    # if @movie.save
+    #   redirect_to @movie
+    # else
+    #   render :new
+    # end
+
+    respond_to do |format|
+      if @movie.save
+        format.html { redirect_to movie_url(@movie), notice: "Movie was successfully created." }
+        format.json { render :show, status: :created, location: @movie }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @movie.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    redirect_to movies_url
+    # redirect_to movies_url
+
+    respond_to do |format|
+      format.html { redirect_to movies_url, alert: "The movie \"#{@movie.title}\" was successfully deleted." }
+      format.json { head :no_content }
+    end
   end
 
 private

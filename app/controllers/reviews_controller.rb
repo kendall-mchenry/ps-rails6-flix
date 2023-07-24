48c20fe6eb0170ab32ac7e1d13b2fd44b6946548
Kendall McHenry <kendall.mchenry@ramseysolutions.com>
2023-07-24 17:15:37 -0500

    Update reviews functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,36 @@
 class ReviewsController < ApplicationController
 
+  before_action :set_movie
+
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
+  end
+
+  def new
+    @review = @movie.reviews.new
+  end
+
+  def create
+    @review = @movie.reviews.new(review_params)
+
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to movie_reviews_path(@movie), notice: "Thanks for your review!" }
+        # format.json { render :show, status: :created, location: @movie }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:name, :comment, :stars)
   end
 end
